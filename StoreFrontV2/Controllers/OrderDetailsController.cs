c1a94e32ac4f9d94e21ffee1e96f756def300841
J-Cundiff <jd.cundiff@outlook.com>
2022-12-06 19:31:35 -0600

    Created Controllers and Views, Edited the crud views including indexes, updated links.

---
+++ StoreFrontV2/Controllers/OrderDetailsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoreFrontV2.DATA.EF.Models;
+
+namespace StoreFrontV2.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        private readonly StoreFrontContext _context;
+
+        public OrderDetailsController(StoreFrontContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderDetails
+        public async Task<IActionResult> Index()
+        {
+            var storeFrontContext = _context.OrderDetails.Include(o => o.Customer).Include(o => o.Product).Include(o => o.Shipper);
+            return View(await storeFrontContext.ToListAsync());
+        }
+
+        // GET: OrderDetails/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails
+                .Include(o => o.Customer)
+                .Include(o => o.Product)
+                .Include(o => o.Shipper)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId");
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            ViewData["ShipperId"] = new SelectList(_context.Shippers, "ShipperId", "ShipperId");
+            return View();
+        }
+
+        // POST: OrderDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,CustomerId,ShipperId,ProductId,ShipperName,ShippedCity,ShippedState,ShippedCountry,OrderDate,ShippingCost")] OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", orderDetail.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", orderDetail.ProductId);
+            ViewData["ShipperId"] = new SelectList(_context.Shippers, "ShipperId", "ShipperId", orderDetail.ShipperId);
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", orderDetail.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", orderDetail.ProductId);
+            ViewData["ShipperId"] = new SelectList(_context.Shippers, "ShipperId", "ShipperId", orderDetail.ShipperId);
+            return View(orderDetail);
+        }
+
+        // POST: OrderDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,CustomerId,ShipperId,ProductId,ShipperName,ShippedCity,ShippedState,ShippedCountry,OrderDate,ShippingCost")] OrderDetail orderDetail)
+        {
+            if (id != orderDetail.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderDetailExists(orderDetail.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", orderDetail.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", orderDetail.ProductId);
+            ViewData["ShipperId"] = new SelectList(_context.Shippers, "ShipperId", "ShipperId", orderDetail.ShipperId);
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.OrderDetails == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetail = await _context.OrderDetails
+                .Include(o => o.Customer)
+                .Include(o => o.Product)
+                .Include(o => o.Shipper)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetail);
+        }
+
+        // POST: OrderDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.OrderDetails == null)
+            {
+                return Problem("Entity set 'StoreFrontContext.OrderDetails'  is null.");
+            }
+            var orderDetail = await _context.OrderDetails.FindAsync(id);
+            if (orderDetail != null)
+            {
+                _context.OrderDetails.Remove(orderDetail);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderDetailExists(int id)
+        {
+          return _context.OrderDetails.Any(e => e.OrderId == id);
+        }
+    }
+}
